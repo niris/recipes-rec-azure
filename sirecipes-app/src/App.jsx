33acf93e7f9c2c1983e7cc0767e90ee756d0b7e3
niris <test@github.local.com>
2023-05-09 23:30:44 +0200

    define env variable for api url

--- sirecipes-app/src/App.jsx
+++ sirecipes-app/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   const [ingredients, setIngredients] = useState([]);
   const [result, setResult] = useState(null);
   const apiUrl = import.meta.env.VITE_API_URL;
+  console.log("Meta env", import.meta.env)
   console.log("API URL", apiUrl)
 
   function handleAddIngredient(event) {
--- sirecipes-app/vite.config.js
+++ sirecipes-app/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import env from 'dotenv'
+//import env from 'dotenv'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),env],
+  plugins: [react()],
 })
